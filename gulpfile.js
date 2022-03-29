3b0da8cf1350741dd53ba62145f6b02b95d3eb4f
riicky-hub <richardjosephace1960@gmail.com>
2022-03-28 23:28:37 -0300

    adicionando gulp

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+// Requires
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const image = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const jsmin = require('gulp-uglify');
+const { parallel } = require('gulp');
+
+// Funções
+function tarefaCSS(callback) {
+    gulp.src(['./src/css/*.css', './node_modules/bootstrap/dist/css/bootstrap.css'])
+        .pipe(concat('styles.css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest("./dist/css"))
+    return callback()
+}
+function tarefaJS(callback) {
+    gulp.src('./node_modules/bootstrap/dist/js/bootstrap.js')
+        .pipe(concat('script.js'))
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/js'))
+    return callback()
+}
+function tarefaHTML(callback) {
+    gulp.src('./src/html/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist/html'))
+    return callback()
+}  
+function tarefaIMG(callback) {
+    gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/img'))
+    return callback()
+}
+
+// Chamadas
+exports.default = parallel(tarefaCSS,tarefaHTML,tarefaJS);
+exports.styles = tarefaCSS;
+exports.scripts = tarefaJS;
+exports.htmls = tarefaHTML;
+exports.images = tarefaIMG;
